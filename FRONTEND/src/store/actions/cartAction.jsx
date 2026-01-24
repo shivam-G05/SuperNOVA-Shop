e53bc8f18dc478fcca9441702b259093468893d3
Shivam Goel <shivamkgjj2005@gmail.com>
2026-01-24 22:26:03 +0530

    Deploy Changes

--- FRONTEND/src/store/actions/cartAction.jsx
+++ FRONTEND/src/store/actions/cartAction.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const asyncaddtocart=async(product_id,product_qty)=>{
     try{
-        const res=await axios.post(`${import.meta.env.VITE_API_URL}/cart/api/cart/items`,{
+        const res=await axios.post(`${import.meta.env.VITE_API_BASE_URL}/cart/api/cart/items`,{
             productId:product_id,
             qty:product_qty,
             
@@ -16,7 +16,7 @@ export const asyncaddtocart=async(product_id,product_qty)=>{
 };
 export const asyncgetcartitems=async()=>{
     try{
-        const res=await axios.get(`${import.meta.env.VITE_API_URL}/cart/api/cart`,{
+        const res=await axios.get(`${import.meta.env.VITE_API_BASE_URL}/cart/api/cart`,{
             withCredentials:true
         });
         return(res.data.cart.items);
@@ -27,7 +27,7 @@ export const asyncgetcartitems=async()=>{
 
 export const asyncupdatecartitems=async(product_id,product_quantity)=>{
     try{
-        const res=await axios.patch( `${import.meta.env.VITE_API_URL}cart/api/cart/items/${product_id}`,{
+        const res=await axios.patch( `${import.meta.env.VITE_API_BASE_URL}cart/api/cart/items/${product_id}`,{
             qty:product_quantity
         },{
             withCredentials:true
